05ab2fc96b20fb024727fae58e9246d29ee15fcf
amasik <nyurise@gmail.com>
2021-03-28 16:45:41 +0200

    Ajout gestion images

---
+++ src/img/Image.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import img from './external.jpg';
+const Images = {
+    0:{date:"27 mars 2021",img:logo},
+    1:{date:"28 mars 2021",img:img}
+  
+};
+
+export default Images;
